d0ed39b5bc7988cfe5a3d4e2f074aeab278d32a3
Vidhyabhushan Balaji <99535514+vidhyabhushanbalaji@users.noreply.github.com>
2025-11-06 19:05:38 +0000

    Multiplication function
    Required slight adjustment from the initial html file, however the site now completes a basic function of receiving three numbers and returning their product.

---
+++ Backend.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+# A simple GET endpoint
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/multiply', methods=['POST'])
+def multiply():
+    data = request.get_json()
+    numbers = data.get('numbers',[])
+    result = float(numbers [0])*float(numbers[1])*float(numbers[2])
+    return jsonify({"result": result})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8501, host='0.0.0.0')
