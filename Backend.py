304032f2a050a7be934477333b784be93e9a55ab
Vidhyabhushan Balaji <99535514+vidhyabhushanbalaji@users.noreply.github.com>
2026-01-07 00:05:55 +0000

    changes made to debug and start to store data
    hopefully bring up a history option soon

--- Backend.py
+++ Backend.py
@@ -9,7 +9,11 @@ def multiply():
     data = request.get_json()
     numbers = data.get('numbers',[])
     result = float(numbers [0])*float(numbers[1])*float(numbers[2])
+    f = open('data.txt','a')
+    f.write('\n')
+    f.write(str(result))
+    f.close()
     return jsonify({"result": result})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8501, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='0.0.0.0')
